22fb725fe8534d4a74d645d9cfd8c5f209be4794
Drnutsu <dentistnut@gmail.com>
2015-02-14 10:15:24 +0700

    jquery add borrower. and add well

--- app/views/library/layout.blade.php
+++ app/views/library/layout.blade.php
@@ -8,6 +8,7 @@
 	<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href=" {{ asset('css/main.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/jquery.dataTables.min.css') }}">
+  
 
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
