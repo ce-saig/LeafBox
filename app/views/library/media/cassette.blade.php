9f2a9a56dc6c23d3c9cbbcee3824f58a4a0077d0
EnceladusIApetus <parallel_worlds@windowslive.com>
2015-07-27 12:18:34 -0400

    [#52] Top section was added to edit all fields of each media.

--- app/views/library/media/cassette.blade.php
+++ app/views/library/media/cassette.blade.php
@@ -7,10 +7,29 @@
 	<div class="panel panel-{{$item->reserved?'warning':'success'}}">
 		<div class="panel-heading">
 			<h3 class="panel-title">
-			{{$item->id}}. {{$book->title}} ({{$item->reserved?"ถูกยืม":"ยืมได้"}})
+				{{$item->id}}. {{$book->title}} ({{$item->reserved?"ถูกยืม":"ยืมได้"}})
 			</h3>
 		</div>
 		<div class="panel-body">
+			<div class="text-center">
+				<div class="input col-md-10">
+					<div class="col-md-1 form-label">สถานะ</div>
+					<div class="col-md-4">
+						<select class="form-control" id="select-all-status">
+							<option value="0">ปกติ</option>
+							<option value="1">ชำรุด</option>
+							<option value="2">รอซ่อม</option>
+						</select>
+					</div>
+					<div class="col-md-1 form-label">หมายเหตุ</div>
+					<div class="col-md-6"><input type="text" name="note[]" class="form-control" id="head-note" value=""></div>
+				</div>
+				<div class="col-md-2">
+					<button type="button" class="btn btn-warning" id="edit-all-field">แก้ไขทั้งหมด</button>
+				</div>
+			</div>
+			<br><br>
+			<hr>
 			<form action="{{ URL::to('book/'.$book->id.'/cassette/'.$item->id.'/edit'); }}" method="POST" role="form">
 				<div class="col-md-12">
 					<table class="table">
@@ -25,14 +44,14 @@
 							<td>{{$value->id}}</td>
 							<td>{{$value->part}}</td>
 							<td>
-								<select name="status[]" class="form-control">
+								<select name="status[]" class="form-control select-status">
 									<option {{$value->status==0?'selected':''}} value="0">ปกติ</option>
 									<option {{$value->status==1?'selected':''}} value="1">ชำรุด</option>
 									<option {{$value->status==2?'selected':''}} value="2">รอซ่อม</option>
 								</select>
 							</td>
 							<td>
-								<input type="text" name="note[]" class="form-control" value="{{$value->notes}}">
+								<input type="text" name="note[]" class="form-control note" value="{{$value->note}}">
 							</td>
 						</tr>
 						@endforeach
@@ -46,4 +65,14 @@
 		</div>
 	</div>
 </div>
+@stop
+
+@section('script')
+@parent
+<script>
+	$('#edit-all-field').click(function() {
+		$('.select-status').prop('value', $('#select-all-status').val());
+		$('.note').prop('value', $('#head-note').val());
+	});
+</script>
 @stop
--- app/views/library/media/cd.blade.php
+++ app/views/library/media/cd.blade.php
@@ -11,6 +11,33 @@
 			</h3>
 		</div>
 		<div class="panel-body">
+			<div class="text-center">
+				<div class="line1">
+					<div class="input col-md-10" >
+						<div class="col-md-2 form-label" style="text-align: left">สถานะ</div>
+						<div class="col-md-3">
+							<select class="form-control" id="select-all-status">
+								<option value="0">ปกติ</option>
+								<option value="1">ชำรุด</option>
+								<option value="2">รอซ่อม</option>
+							</select>
+						</div>
+						<div class="col-md-2 form-label">หมายเหตุ</div>
+						<div class="col-md-5"><input type="text" name="note[]" class="form-control" id="head-note" value=""></div>	
+					</div>
+					<div class="col-md-2">
+						<button type="button" class="btn btn-warning" id="edit-all-field">แก้ไขทั้งหมด</button>
+					</div>
+				</div>
+				<br><br><br>
+				<div class="line2 row" id="cd-field">
+					<div class="col-md-2 form-label">แทร็กเริ่มต้น</div>
+					<div class="col-md-2"><input type="number" name="track_fr[]" class="form-control" id="tr-start" value="0"></div>
+					<div class="col-md-2 form-label" id="cd-field-label">แทร็กสุดท้าย</div>
+					<div class="col-md-2" id="cd-field-input"><input type="number" name="track_to[]" class="form-control" id="tr-end" value="0"></div>
+				</div>
+			</div>
+			<hr>
 			<form action="{{ URL::to('book/'.$book->id.'/cd/'.$item->id.'/edit'); }}" method="POST" role="form">
 				<div class="col-md-12">
 					<table class="table">
@@ -27,20 +54,20 @@
 							<td>{{$value->id}}</td>
 							<td>{{$value->part}}</td>
 							<td>
-								<select name="status[]" class="form-control">
+								<select name="status[]" class="form-control select-status">
 									<option {{$value->status==0?'selected':''}} value="0">ปกติ</option>
 									<option {{$value->status==1?'selected':''}} value="1">ชำรุด</option>
 									<option {{$value->status==2?'selected':''}} value="2">รอซ่อม</option>
 								</select>
 							</td>
 							<td>
-								<input type="text" name="note[]" class="form-control" value="{{$value->notes}}">
+								<input type="text" name="note[]" class="form-control note" value="{{$value->notes}}">
 							</td>
 							<td>
-								<input type="number" name="track_fr[]" class="form-control" value="{{$value->track_fr}}">
+								<input type="number" name="track_fr[]" class="form-control track-start" value="{{$value->track_fr}}">
 							</td>
 							<td>
-								<input type="number" name="track_to[]" class="form-control" value="{{$value->track_to}}">
+								<input type="number" name="track_to[]" class="form-control track-end" value="{{$value->track_to}}">
 							</td>
 						</tr>
 						@endforeach
@@ -54,4 +81,16 @@
 		</div>
 	</div>
 </div>
+@stop
+
+@section('script')
+@parent
+<script>
+	$('#edit-all-field').click(function() {
+		$('.select-status').prop('value', $('#select-all-status').val());
+		$('.note').prop('value', $('#head-note').val());
+		$('.track-start').prop('value', $('#tr-start').val());
+		$('.track-end').prop('value', $('#tr-end').val());
+	});
+</script>
 @stop
